043af1c58bc09a4a359a485b43f8619b8ac4f69d
phoenixpei <ppei@deloitte.com>
2015-02-11 11:26:02 -0500

    made changes on codes with description and hashing pw

--- AnomalyDetectionScript.R
+++ AnomalyDetectionScript.R
@@ -1,18 +1,23 @@
 ##################################################################
-##project name:
-##modified by:
-##date modified:
-##comments:
+##project name:Hydra R Anomaly Detection
+##modified by:Phoenix Pei
+##date modified:2/11/2015
+##comments:The query below pulls data from PostgreSQL optiontsnapshot table and conduct Anomaly Detectioin analysis with R. In the end, a plotly function is used to plot the result to https://plot.ly/~phoenixpei/48
 ##################################################################
 
-souinstall.packages ("RPostgreSQL")
+install.packages ("RPostgreSQL")
 install.packages ("DBI")
 install.packages ("fda")
 install.packages("devtools")
 install.packages ("reshape", "chron")
+##pp: install package for hashing password
+if (!require("devtools")) install.packages("devtools")
+
 
 ##fmk: need clarification on model that is being used
 devtools::install_github("twitter/AnomalyDetection")
+##pp: load scrypt package for password hashing
+devtools::install_github("rstudio/rscrypt")
 library(AnomalyDetection)
 library (RPostgreSQL)
 library (DBI)
@@ -21,9 +26,13 @@ library (chron)
 
 drv = dbDriver("PostgreSQL")
 ##fmk: password needs to get hashed through another function
+##pp: hash password
+hashed1 <- scrypt::hashPassword("Ready2go")
+scrypt::verifyPassword(hashed1, "Ready2go")
+
 con = dbConnect(drv, user = "ppei", password = "Ready2go", dbname = "QTRACK", host = "50.168.76.47", port = 5432)
 rs <- dbGetQuery (con, "select load_date || ' ' || load_time, call_implied_volatility from optionsnapshot where load_date='2015-01-16' order by cast(load_time as time)")
-rs <- dbGetQuery (con, "select load_date || ' ' || load_time, call_implied_volatility from optionsnapshot where load_date='2015-01-16' order by load_time")
+
 dt <- as.data.frame(rs)
 AnomalyDetectionVec(dt[,2], max_anoms=0.2, period=10, direction='both', only_last=FALSE, plot=TRUE)
 
@@ -45,6 +54,8 @@ install_github("ropensci/plotly")
 library(plotly)
 
 ##fmk: password needs to get hashed through another function
+##pp: hash password
+hashed2 <- scrypt::hashPassword("xylj590nlr")
 set_credentials_file("phoenixpei","xylj590nlr")
 py <- plotly()
 
@@ -55,6 +66,6 @@ response <- py$plotly(data,kwargs=list(filename="Anomalies Detection", fileopt="
 browseURL(response$url)
 
 ##fmk: additional section needs to be created to store plot results into a postgres table
-
+##pp: work on refining result to make sure results are validate so that can be written into a postgresql table
 
 
