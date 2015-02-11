c6c12b646dbab6807314134925b1e0d08fc31b6e
phoenixpei <ppei@deloitte.com>
2015-02-11 17:42:37 -0500

    change 2/11/15 5pm

--- AnomalyDetectionScript.R
+++ AnomalyDetectionScript.R
@@ -31,20 +31,13 @@ hashed1 <- scrypt::hashPassword("Ready2go")
 scrypt::verifyPassword(hashed1, "Ready2go")
 
 con = dbConnect(drv, user = "ppei", password = "Ready2go", dbname = "QTRACK", host = "50.168.76.47", port = 5432)
-rs <- dbGetQuery (con, "select load_date || ' ' || load_time, call_implied_volatility from optionsnapshot where load_date='2015-01-16' order by cast(load_time as time)")
+rs <- dbGetQuery (con, "select (load_date || ' ' || load_time)::timestamp, call_implied_volatility from optionsnapshot where load_date='2015-01-16' order by cast(load_time as time)")
 
 dt <- as.data.frame(rs)
-AnomalyDetectionVec(dt[,2], max_anoms=0.2, period=10, direction='both', only_last=FALSE, plot=TRUE)
+AnomalyDetectionVec(dt[,2], max_anoms=0.2, period=7, direction='both', only_last=FALSE, plot=TRUE)
 
 res = AnomalyDetectionTs(rs, max_anoms=0.02, direction='both', plot=TRUE)
 res$plot
-
-
-#####
-rs <- dbSendQuery (con, "select load_time, call_implied_volatility from optionsnapshot where load_date='2015-01-21' order by load_time")
-df <- fetch(rs, n= -1)
-dt <- as.data.frame(df)
-AnomalyDetectionVec(dt[,2], max_anoms=0.2, period=10, direction='both', only_last=FALSE, plot=TRUE)
 
 
 #####
@@ -55,10 +48,11 @@ library(plotly)
 
 ##fmk: password needs to get hashed through another function
 ##pp: hash password
-hashed2 <- scrypt::hashPassword("xylj590nlr")
+hashed2 <- scrypt::hashPassword("xylj590nlr") 
 set_credentials_file("phoenixpei","xylj590nlr")
 py <- plotly()
 
+##pp: dt[,1] includes timestamp. needs to find the right way to bring it to data parameter
 data <- list( list(x=dt[,1], y=dt[,2]))
 
 response <- py$plotly(data,kwargs=list(filename="Anomalies Detection", fileopt="overwrite"))
