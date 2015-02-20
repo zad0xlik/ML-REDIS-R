7c47bd90034e85d8407dceb7a8a30465a960a9e5
phoenixpei <ppei@deloitte.com>
2015-02-20 13:57:15 -0500

    update rpart code 2/20 2pm

--- RPart.R
+++ RPart.R
@@ -1,23 +1,28 @@
 install.packages('rattle')
 install.packages('rpart.plot')
 install.packages('RColorBrewer')
+install.packages('xlsx')
+
 library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
+library(xlsx)
 
-mydata <- read.csv("Workbook1.csv")
-mydata <- mydata[1:50,]
+setwd("/Professional/CODES")
+mydata <- read.xlsx("Workbook.xlsx", 1) # change tabs
+##mydata <- mydata[1:40,]
 
 attach(mydata)
 
 ## Split data into testing and training using
-set.seed(12)
+set.seed(1234)
 train <- sample(2, nrow(mydata), replace=TRUE, prob=c(0.7, 0.3))
+
 test <- -train
 training_data <- mydata[train,]
 testing_data <- mydata[test,]
 
-rpart_model <- rpart(qty ~ call_option_symbol + call_bid_ask_size, data=training_data, method="class",control=rpart.control(minsplit=20))
+rpart_model <- rpart(qty ~., data=training_data, method="class",control=rpart.control(minsplit=20, cp=0))
 attributes(rpart_model)
 print(rpart_model)
 fancyRpartPlot(rpart_model, uniform=TRUE, main="Classification Tree for Optionsnapshot")
