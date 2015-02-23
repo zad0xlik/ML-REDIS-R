01d8476d9fd9aaac7623abd0344d2bc777cb2bd4
phoenixpei <ppei@deloitte.com>
2015-02-23 16:42:24 -0500

    update tree code 2/23 4:40pm

--- RPart.R
+++ RPart.R
@@ -8,13 +8,13 @@ library(rpart.plot)
 library(RColorBrewer)
 library(xlsx)
 
-setwd("/Professional/CODES")
+setwd("/Professional/CODES") # set dir
 mydata <- read.xlsx("Workbook.xlsx", 1) # change tabs
 mydata <- mydata[1:40,]
 
 attach(mydata)
 
-rpart_model <- rpart(qty ~., data=mydata, method="class",control=rpart.control(minsplit=20, cp=0))
+rpart_model <- rpart(qty ~ call_option_symbol + call_bid_ask_size, data=mydata, method="class",control=rpart.control(minsplit=20, cp=0))
 par(mfrow = c(1,2), xpd = TRUE)
 
 ##attributes(rpart_model)
