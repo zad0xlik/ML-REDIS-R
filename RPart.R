94807b106bc8141ef11124527fe7b9e4f9cf4229
ppei <fkolyadin@deloitte.com>
2015-02-23 18:08:46 -0800

    merging code

--- RPart.R
+++ RPart.R
@@ -3,7 +3,7 @@
 ##install.packages('RColorBrewer')
 ##install.packages('xlsx')
 ##install.packages('party')
-install.packages('randomForest')
+##install.packages('randomForest')
 
 library(rattle)
 library(rpart.plot)
@@ -14,23 +14,23 @@ library(strucchange) # For the sctest function to extract p-values (see help for
 library(randomForest) # readme - rfNews()
 
 
-<<<<<<< e5f0a1411120735bcbe2f9edca01c178e6d90b49
-setwd("/Professional/CODES") # set dir
-mydata <- read.xlsx("Workbook.xlsx", 1) # change tabs
-mydata <- mydata[1:40,]
-
-attach(mydata)
-
-rpart_model <- rpart(qty ~ call_option_symbol + call_bid_ask_size, data=mydata, method="class",control=rpart.control(minsplit=20, cp=0))
-par(mfrow = c(1,2), xpd = TRUE)
-
-##attributes(rpart_model)
-print(rpart_model)
-##plot(rpart_model, uniform=T)
-fancyRpartPlot(rpart_model, uniform=TRUE, main="Classification Tree for Optionsnapshot")
-text(rpart_model, use.n=TRUE, all=TRUE, cex=.8)
-
-detach(mydata)
+# <<<<<<< e5f0a1411120735bcbe2f9edca01c178e6d90b49
+# setwd("/Professional/CODES") # set dir
+# mydata <- read.xlsx("Workbook.xlsx", 1) # change tabs
+# mydata <- mydata[1:40,]
+# 
+# attach(mydata)
+# 
+# rpart_model <- rpart(qty ~ call_option_symbol + call_bid_ask_size, data=mydata, method="class",control=rpart.control(minsplit=20, cp=0))
+# par(mfrow = c(1,2), xpd = TRUE)
+# 
+# ##attributes(rpart_model)
+# print(rpart_model)
+# ##plot(rpart_model, uniform=T)
+# fancyRpartPlot(rpart_model, uniform=TRUE, main="Classification Tree for Optionsnapshot")
+# text(rpart_model, use.n=TRUE, all=TRUE, cex=.8)
+# 
+# detach(mydata)
 =======
 setwd("C:/Users/fkolyadin/Desktop/r_anomaly_detect")
 mydata <- read.xlsx("tree_1.xlsx", 1) # change tabs
